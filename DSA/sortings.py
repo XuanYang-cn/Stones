68a717ca04654698a7c25855d2c66e46b044359b
Yang Xuan <jumpthepig@gmail.com>
2019-08-20 20:25:47 +0800

    add selection sort, insertion sort and quick sort

--- DSA/sortings.py
+++ DSA/sortings.py
@@ -37,6 +37,80 @@ class Sorting:
                 if nums[j+1] < nums[j]:
                     nums[j+1], nums[j] = nums[j], nums[j+1]
 
+    @classmethod
+    @time_it
+    def selection_sort(cls, nums):
+        '''
+        Time: O(N^2)
+        In-place
+        Not stable
+        Method: find the smallest and swap
+        '''
+        for i in range(len(nums)-1):
+            index = i  # i is the first number to be sort at the moment
+
+            for j in range(i+1, len(nums)):
+                if nums[j] < nums[index]:
+                    index = j
+
+            if index != i:
+                nums[i], nums[index] = nums[index], nums[i]
+
+    @classmethod
+    @time_it
+    def insertion_sort(cls, nums):
+        '''
+        Time: O(N^2)
+        In-place
+        Stable
+        Method: If smaller shift
+        '''
+        for i in range(1, len(nums)):
+            j = i
+            while j > 0 and nums[j-1] > nums[j]:
+                nums[j-1], nums[j] = nums[j], nums[j-1]
+                j -= 1
+
+
+    @classmethod
+    def _quick_sort_partition(cls, nums, low, high):
+        pivot_index = (low + high) // 2
+        nums[pivot_index], nums[high] = nums[high], nums[pivot_index]
+
+        index = low
+        for i in range(low, high):
+            if nums[i] < nums[high]:
+                nums[i], nums[index] = nums[index], nums[i]
+                index += 1
+
+        nums[index], nums[high] = nums[high], nums[index]
+        return index
+
+    @classmethod
+    def _quick_sort(cls, nums, low, high):
+        if low >= high:
+            return
+
+        pivot  = cls._quick_sort_partition(nums, low, high)
+        cls._quick_sort(nums, low, pivot-1)
+        cls._quick_sort(nums, pivot+1, high)
+
+    @classmethod
+    @time_it
+    def quick_sort(cls, nums):
+        '''
+        Time: O(NlogN)
+        In-place
+        Not stable
+        Method: divide and conquer algorithm
+            pick an element from the array: pivot
+            partition phase: rearrange the array so that all elements with value less
+                than pivot item come before the pivot, while all elements with values
+                greater than the pivot come after it, equal elements can go either way.
+        '''
+        cls._quick_sort(nums, 0, len(nums)-1)
+
+
 def gen(num):
     '''generate list with random integers of num numbers'''
     return [random.randint(0, 100) for i in range(num)]
@@ -51,11 +125,25 @@ def is_sorted(nums):
 
 
 def tests():
+    # Test bubble sort
     test_nums = gen(100)
     Sorting.bubble_sort(test_nums)
     assert is_sorted(test_nums)
 
+    # Test selection sort
+    test_nums01 = gen(100)
+    Sorting.selection_sort(test_nums01)
+    assert is_sorted(test_nums01)
+
+    # Test insertion sort
+    test_nums02 = gen(100)
+    Sorting.insertion_sort(test_nums02)
+    assert is_sorted(test_nums02)
+
+    # Test quick sort
+    test_nums03 = gen(100)
+    Sorting.quick_sort(test_nums03)
+    assert is_sorted(test_nums03)
 
 if __name__ == '__main__':
     tests()
-
