9c931e6979a9ec31da548e33492b7b3c30669cf9
yangxuan <xuan.yang@zilliz.com>
2021-03-26 14:39:21 +0800

    add golang

---
+++ Golang/pulsar_example/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"github.com/apache/pulsar-client-go/pulsar"
+
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	go func() {
+		fmt.Println(http.ListenAndServe("localhost:9876", nil))
+	}()
+
+	newClient := func() {
+		var client pulsar.Client
+
+		opts := pulsar.ClientOptions{
+			URL: "pulsar://127.0.0.1:6650",
+		}
+
+		client, err := pulsar.NewClient(opts)
+		if err != nil {
+			panic(err)
+		}
+
+		channelName := "insert"
+		pp, err := client.CreateProducer(pulsar.ProducerOptions{Topic: channelName})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("producer: ", pp)
+
+		receiveChannel := make(chan pulsar.ConsumerMessage, 1024)
+		pc, err := client.Subscribe(pulsar.ConsumerOptions{
+			Topic:                       channelName,
+			SubscriptionName:            channelName,
+			Type:                        pulsar.KeyShared,
+			SubscriptionInitialPosition: pulsar.SubscriptionPositionEarliest,
+			MessageChannel:              receiveChannel,
+		})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("consumer: ", pc)
+		pp.Close()
+		pc.Close()
+
+		client.Close()
+	}
+
+	sc := make(chan os.Signal, 1)
+
+	go func() {
+		for i := 0; i < 1; i++ {
+			newClient()
+		}
+	}()
+
+	signal.Notify(sc,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+
+	sig := <-sc
+	fmt.Println("Get signal to exit", sig.String())
+
+}
